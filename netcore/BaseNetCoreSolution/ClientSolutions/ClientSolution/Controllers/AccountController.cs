2ab710f903b5e69a07e59ede4dc2de84663191bd
MuammerKrc <muammer03karaca@gmail.com>
2022-08-21 22:29:13 +0300

    addded shared layers netcore

---
+++ netcore/BaseNetCoreSolution/ClientSolutions/ClientSolution/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace ClientSolution.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            var baseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+
+                new Claim(ClaimTypes.Email, "muammer03karaca@gmail.com"),
+            };
+            var licenseClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "other"),
+
+                new Claim(ClaimTypes.Email, "muammer03karaca@gmail.com"),
+            };
+            var grandmaIdentity = new ClaimsIdentity(baseClaims,"grandma Identity");
+            var licenceIdentity = new ClaimsIdentity(licenseClaims, "licence Identity");
+            var userPrincipal = new ClaimsPrincipal(new []{grandmaIdentity, licenceIdentity });
+            HttpContext.SignInAsync(userPrincipal);
+            return View();
+        }
+    }
+}
---
+++ netcore/BaseNetCoreSolution/SharedLayers/SharedServiceLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedServiceLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
